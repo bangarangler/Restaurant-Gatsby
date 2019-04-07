0fd7ca8de3621f5141cd5815894dc362de207d50
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-04-07 15:09:42 -0400

    Added SectionTitle and Section component
    styled it and have to remember to pass in children

--- src/components/HomePageComponents/QuickInfo.js
+++ src/components/HomePageComponents/QuickInfo.js
@@ -1,11 +1,13 @@
 import React from "react"
+import { Section } from "../../utils/Section.js"
+import SectionTitle from "../../utils/SectionTitle.js"
 
 export default class QuickInfo extends React.Component {
   render() {
     return (
-      <div>
-        <p>content of quick info</p>
-      </div>
+      <Section>
+        <SectionTitle message="let us tell you" title="our mission" />
+      </Section>
     )
   }
 }
---
+++ src/utils/SectionTitle.js
@@ -0,0 +1,19 @@
+import React from "react"
+import styles from "./Section.module.less"
+
+const SectionTitle = ({ title, message }) => {
+  return (
+    <div className={styles.TitleWrapper}>
+      <h3 className={styles.message}>{message}</h3>
+      <h1 className={styles.title}>{title}</h1>
+      <div className={styles.underline} />
+    </div>
+  )
+}
+
+SectionTitle.defaultProps = {
+message: 'Our message',
+title: 'out title'
+}
+
+export default SectionTitle
